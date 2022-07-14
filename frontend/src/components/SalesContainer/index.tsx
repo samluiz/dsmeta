e324c716451d2287edf1ba598e21d2d30de6b57b
samluiz <samuelluizrl@outlook.com>
2022-07-14 00:15:03 -0300

    Other Components and styling

--- frontend/src/components/SalesContainer/index.tsx
+++ frontend/src/components/SalesContainer/index.tsx
@@ -7,6 +7,10 @@ const Container = styled.div`
   height: 100vh;
   margin: 3.75rem;
   border-radius: 10px;
+  -webkit-box-shadow: 4px 6px 41px -12px rgba(0,0,0,0.68);
+  -moz-box-shadow: 4px 6px 41px -12px rgba(0,0,0,0.68);
+  box-shadow: 4px 6px 41px -12px rgba(0,0,0,0.68);
+  
 `
 
 const SalesContainer: React.FC = () => {
