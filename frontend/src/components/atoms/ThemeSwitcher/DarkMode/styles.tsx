f898277b92728a3d922c97197bece281997ef121
samluiz <samuelluizrl@outlook.com>
2022-07-15 09:45:38 -0300

    Database

--- frontend/src/components/atoms/ThemeSwitcher/DarkMode/styles.tsx
+++ frontend/src/components/atoms/ThemeSwitcher/DarkMode/styles.tsx
@@ -1,33 +1,32 @@
-import styled, { ThemeConsumer } from "styled-components";
-
+import styled, { ThemeConsumer } from 'styled-components';
 
 const DarkModeIcon = styled.div`
-  position: fixed;
-  width: 90%;
-  overflow: hidden;
-  top: 2rem;
-  left: 0;
+    position: fixed;
+    width: 90%;
+    overflow: hidden;
+    top: 2rem;
+    left: 0;
 
-  button {
-    padding: 0.8rem;
-    border-radius: 100%;
-    background: ${({theme}) => theme.bgcolor};
-    border: none;
-    border-radius: 0 20px 20px 0;
-    cursor: pointer;
-    transition-duration: 0.2s;
-    &:hover {
-      background: black;
-      .icon {
-        filter: invert(100%);
-      }
+    button {
+        padding: 0.8rem;
+        border-radius: 100%;
+        background: ${({ theme }) => theme.bgcolor};
+        border: none;
+        border-radius: 0 20px 20px 0;
+        cursor: pointer;
+        transition-duration: 0.2s;
+        &:hover {
+            background: black;
+            .icon {
+                filter: invert(100%);
+            }
+        }
     }
-  }
 
-  .icon {
-    width: 30px;
-    height: 30px;
-  }
-`
+    .icon {
+        width: 30px;
+        height: 30px;
+    }
+`;
 
 export default DarkModeIcon;
